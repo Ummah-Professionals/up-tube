3e3760a47cf01538252862712d6bc3e44ff67454
amaangadatia <amaangadatia2k@gmail.com>
2024-07-08 12:33:52 -0400

    Called API on sample wp

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,9 +1,40 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { slowFetchJson } from "../utilities";
 
 export const Careers = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["apiData"],
+    queryFn: () => slowFetchJson("/api").then((json) => json.message),
+  });
+
+
+  const renderContent = () => {
+    if (isPending) {
+      return <p>Loading, please wait...</p>;
+    }
+
+    if (error) {
+      return (
+        <p>
+          Got an error: <b>{error.message}</b>
+        </p>
+      );
+    }
+
+    return (
+      <p>
+        Our company motto is:
+        <span className="api-text"> {data}</span>
+      </p>
+    );
+  };
+
   return (
     <main>
       <h1>Careers page</h1>
+
+      {renderContent()}
 
       <p>Here is a list of career areas we offer jobs in:</p>
       <li>Finance</li>
