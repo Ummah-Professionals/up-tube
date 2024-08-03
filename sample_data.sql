a5c1e0b39c9ece8ec16d4dc1f94fcdcc2ab9f9d0
amaangadatia <amaangadatia2k@gmail.com>
2024-08-03 13:32:14 -0400

    Corrected SQL sample data script

--- sample_data.sql
+++ sample_data.sql
@@ -9,25 +9,39 @@ SELECT
 FROM generate_series(1, 100) AS s(i);
 
 -- Create sample data for Videos table with real URLs for thumbnails and video paths
+WITH RandomUsers AS (
+    SELECT id AS user_id,
+           ROW_NUMBER() OVER () AS rn
+    FROM Users
+    ORDER BY RANDOM()
+),
+VideoData AS (
+    SELECT
+        'Video Title ' || s.i AS title,
+        ru.user_id,
+        FLOOR(RANDOM() * 600 + 60) AS duration_seconds, -- Duration between 60 and 660 seconds
+        NOW() - INTERVAL '1 day' * FLOOR(RANDOM() * 365) AS time_uploaded, -- Random date within the last year
+        'https://picsum.photos/' || s.i || '/10/200/300' AS thumbnail,
+        CASE
+            WHEN s.i % 10 = 1 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
+            WHEN s.i % 10 = 2 THEN 'https://www.w3schools.com/html/movie.mp4'
+            WHEN s.i % 10 = 3 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
+            WHEN s.i % 10 = 4 THEN 'https://www.w3schools.com/html/movie.mp4'
+            WHEN s.i % 10 = 5 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
+            WHEN s.i % 10 = 6 THEN 'https://www.w3schools.com/html/movie.mp4'
+            WHEN s.i % 10 = 7 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
+            WHEN s.i % 10 = 8 THEN 'https://www.w3schools.com/html/movie.mp4'
+            WHEN s.i % 10 = 9 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
+            ELSE 'https://www.w3schools.com/html/movie.mp4'
+        END AS video_path,
+        FLOOR(RANDOM() * 10000) AS num_views, -- Random number of views between 0 and 9999
+        'This is the description for video ' || s.i AS description,
+        ROW_NUMBER() OVER () AS row_num
+    FROM generate_series(1, 500) AS s(i)
+    CROSS JOIN RandomUsers ru
+    WHERE ru.rn = (s.i % (SELECT COUNT(*) FROM RandomUsers) + 1)
+)
 INSERT INTO Videos (title, user_id, duration_seconds, time_uploaded, thumbnail, video_path, num_views, description)
-SELECT
-    'Video Title ' || i,
-    (SELECT id FROM Users ORDER BY RANDOM() LIMIT 1),
-    FLOOR(RANDOM() * 600 + 60), -- Duration between 60 and 660 seconds
-    NOW() - INTERVAL '1 day' * FLOOR(RANDOM() * 365), -- Random date within the last year
-    'https://picsum.photos/' || i || '/10/200/300',
-    CASE
-        WHEN i % 10 = 1 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
-        WHEN i % 10 = 2 THEN 'https://www.w3schools.com/html/movie.mp4'
-        WHEN i % 10 = 3 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
-        WHEN i % 10 = 4 THEN 'https://www.w3schools.com/html/movie.mp4'
-        WHEN i % 10 = 5 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
-        WHEN i % 10 = 6 THEN 'https://www.w3schools.com/html/movie.mp4'
-        WHEN i % 10 = 7 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
-        WHEN i % 10 = 8 THEN 'https://www.w3schools.com/html/movie.mp4'
-        WHEN i % 10 = 9 THEN 'https://www.w3schools.com/html/mov_bbb.mp4'
-        ELSE 'https://www.w3schools.com/html/movie.mp4'
-    END,
-    FLOOR(RANDOM() * 10000), -- Random number of views between 0 and 9999
-    'This is the description for video ' || i
-FROM generate_series(1, 500) AS s(i);
+SELECT title, user_id, duration_seconds, time_uploaded, thumbnail, video_path, num_views, description
+FROM VideoData;
+
