003e372f6ddfce58ba02a008632448c7957eb1e1
Harun <harunxharuka@gmail.com>
2024-08-23 15:12:26 -0400

    Load comp, edited watch page,push youssef updates

---
+++ src/components/Load.jsx
@@ -0,0 +1,11 @@
+import './Load.css'; 
+
+const Load = () => (
+  <div className="spinner">
+    <div className="spinner-border" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  </div>
+);
+
+export default Load;
